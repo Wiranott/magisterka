6ca7518ff1748236a607efb6a906254adcf3754a
Mateusz Kotowski <mateuszkotowski1999@gmail.com>
2023-08-08 20:01:44 +0200

    Rozszerzenie generatora checkboxów

--- PdfBoxMgr/src/main/java/pl/example/pdfboxmgr/generator/CheckboxPdfGenerator.java
+++ PdfBoxMgr/src/main/java/pl/example/pdfboxmgr/generator/CheckboxPdfGenerator.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class CheckboxPdfGenerator {
 
     private static final String BASE_PATH = "C:\\Users\\mateu\\OneDrive\\Dokumenty\\magisterka\\magisterka\\PdfBoxMgr\\src\\main\\resources\\pdfs\\";
 
-    public void generatePdfWithCheckbox(String fileName) {
+    public void generatePdfWithCheckbox(String fileName, float xPosition, float yPosition, boolean isChecked) {
         var fullPath = BASE_PATH + fileName;
         try (PDDocument document = new PDDocument()) {
             var page = new PDPage(PDRectangle.A4);
@@ -27,13 +28,17 @@ public class CheckboxPdfGenerator {
             var checkBox = new PDCheckBox(acroForm);
             checkBox.setPartialName("SampleCheckBox");
             var widget = checkBox.getWidgets().get(0);
-            var rect = new PDRectangle(50, 750, 20, 20);
+            var rect = new PDRectangle(xPosition, yPosition, 20, 20);
             widget.setRectangle(rect);
             widget.setPage(page);
             page.getAnnotations().add(widget);
             acroForm.getFields().add(checkBox);
 
-            checkBox.check();
+            if (isChecked) {
+                checkBox.check();
+            } else {
+                checkBox.unCheck();
+            }
 
             document.save(fullPath);
         } catch (IOException e) {
