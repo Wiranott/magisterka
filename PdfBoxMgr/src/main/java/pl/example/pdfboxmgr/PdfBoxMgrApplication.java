ff4fe8c8060ae7567613f8f33cec18451ba2486e
Mateusz Kotowski <mateuszkotowski1999@gmail.com>
2023-08-08 14:21:34 +0200

    Utworzenie projektu
    Utworzenie i odpalenie projektu i dodanie PdfBox

---
+++ PdfBoxMgr/src/main/java/pl/example/pdfboxmgr/PdfBoxMgrApplication.java
@@ -0,0 +1,13 @@
+package pl.example.pdfboxmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfBoxMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfBoxMgrApplication.class, args);
+	}
+
+}
---
+++ PdfBoxMgr/src/test/java/pl/example/pdfboxmgr/PdfBoxMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.example.pdfboxmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfBoxMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
